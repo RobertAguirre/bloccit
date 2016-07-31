6924351904636e5d67b96dcbd5309b3dfe1e9a22
Robert Aguirre <raguirre.cofc@gmail.com>
2016-07-31 19:25:57 +0000

    Checkpoint 36:Completed

--- bloccit/db/schema.rb
+++ bloccit/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727211121) do
+ActiveRecord::Schema.define(version: 20160731190246) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -56,6 +56,14 @@ ActiveRecord::Schema.define(version: 20160727211121) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
