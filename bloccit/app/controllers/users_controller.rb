9785d0553018b98aff709a4ab3290632f9725a66
Robert Aguirre <raguirre.cofc@gmail.com>
2016-07-31 22:32:44 +0000

    Checkpoint 37 Assignment prelim submission

--- bloccit/app/controllers/users_controller.rb
+++ bloccit/app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
    def new
      @user = User.new
    end
+
+   def confirm
+     @user = User.new
+     @user.name = params[:user][:name]
+     @user.email = params[:user][:email]
+   end
    
    def create
  # #9
