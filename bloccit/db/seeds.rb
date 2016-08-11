e58e42667db32d560a1439cf00bb030591dc33be
Robert Aguirre <raguirre.cofc@gmail.com>
2016-08-11 05:33:41 +0000

    Checkpoint 39:Completed WIP

--- bloccit/db/seeds.rb
+++ bloccit/db/seeds.rb
@@ -8,6 +8,16 @@
 
  require 'random_data'
  
+ #Create Users
+  5.times do
+   User.create!(
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all
+ 
  # Create Topics
  15.times do
    Topic.create!(
@@ -19,9 +29,8 @@
 
  # Create Posts
  50.times do
- # 1
    Post.create!(
- # 2
+    user: users.sample,
     topic: topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
@@ -30,16 +39,21 @@
  posts = Post.all
  
  # Create Comments
- #3
  100.times do
    Comment.create!(
- #4
      post: posts.sample,
      body: RandomData.random_paragraph
    )
  end
  
+ user = User.first
+ user.update_attributes!(
+   email: 'Raguirre.cofc@gmail.com', 
+   password: 'helloworld'
+   )
+ 
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
