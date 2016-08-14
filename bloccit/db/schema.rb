e92d78fa9aa254905779bc43023e735dedc2ea98
Robert Aguirre <raguirre.cofc@gmail.com>
2016-08-14 16:34:52 +0000

    Checkpoint 40: Authorization

--- bloccit/db/schema.rb
+++ bloccit/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811010942) do
+ActiveRecord::Schema.define(version: 20160814151801) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20160811010942) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
--- bloccit/db/seeds.rb
+++ bloccit/db/seeds.rb
@@ -46,14 +46,24 @@
    )
  end
  
- user = User.first
- user.update_attributes!(
-   email: 'Raguirre.cofc@gmail.com', 
+#Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ 
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
-   )
+ )
  
  puts "Seed finished"
  puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+
