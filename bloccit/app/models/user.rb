f711bcec03fe6646c2d4db4aa410732da2b1084e
Robert Aguirre <raguirre.cofc@gmail.com>
2016-08-14 21:05:16 +0000

    Checkpoint 41:Completed

--- bloccit/app/models/user.rb
+++ bloccit/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
  
   has_many :posts
+  has_many :comments
  # #2
   before_save { self.email = email.downcase if email.present? }
   before_save { self.role ||= :member }
