d9ed0ba31dba3b04d53bd1c0e4559b7490f23a95
Robert Aguirre <raguirre.cofc@gmail.com>
2016-09-18 17:48:00 +0000

    Checkpoint 45: Public Profiles

--- bloccit/config/routes.rb
+++ bloccit/config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   
   
   
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
